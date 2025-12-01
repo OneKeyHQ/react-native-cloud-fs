5ce65632f58882da7ab6f26b703bc12fa38111ee
morizon <sidmorizon@outlook.com>
2025-12-01 23:35:07 +0800

    fix: google drive login (#7)
    * Change GoogleSignInOptions to request DRIVE_FILE scope
    
    Updated GoogleSignInOptions to request DRIVE_FILE scope instead of DRIVE_APPDATA.
    
    * Update package.json

--- android/src/main/java/org/rncloudfs/RNCloudFsModule.java
+++ android/src/main/java/org/rncloudfs/RNCloudFsModule.java
@@ -393,8 +393,8 @@ public class RNCloudFsModule extends ReactContextBaseJavaModule implements Googl
         GoogleSignInOptions signInOptions =
                 new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                         .requestEmail()
-                        // .requestScopes(new Scope(DriveScopes.DRIVE_FILE))
-                        .requestScopes(new Scope(DriveScopes.DRIVE_APPDATA))
+                        .requestScopes(new Scope(DriveScopes.DRIVE_FILE))
+                        // .requestScopes(new Scope(DriveScopes.DRIVE_APPDATA))
                         .build();
         GoogleSignInClient client = GoogleSignIn.getClient(this.reactContext, signInOptions);
 
@@ -427,8 +427,8 @@ public class RNCloudFsModule extends ReactContextBaseJavaModule implements Googl
         GoogleSignInOptions signInOptions =
                 new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                         .requestEmail()
-                        // .requestScopes(new Scope(DriveScopes.DRIVE_FILE))
-                        .requestScopes(new Scope(DriveScopes.DRIVE_APPDATA))
+                        .requestScopes(new Scope(DriveScopes.DRIVE_FILE))
+                        // .requestScopes(new Scope(DriveScopes.DRIVE_APPDATA))
                         .build();
         GoogleSignInClient client = GoogleSignIn.getClient(this.reactContext, signInOptions);
         mDriveServiceHelper = null;
